9583375dac9eef77a51ce4c8eb80e55536bfa3b6
ProtulSikder <protul.51@gmail.com>
2018-08-03 20:50:45 +0600

    Update Get_Unused_IP.py

--- Get_Unused_IP.py
+++ Get_Unused_IP.py
@@ -1,10 +1,26 @@
-import socket
+import ipaddress,netifaces,subprocess,socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
 s.connect(("www.google.com",80))
 
-myaddr = s.getsockname()[0]
+my_ip = s.getsockname()[0]
 
-s.close()
+for i in netifaces.interfaces():
+	if netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'] == my_ip:
+		my_subnet = netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask']
+		iface = i
 
-print(myaddr)
+cidr = sum([bin(int(i)).count("1") for i in my_subnet.split('.')])
+
+full_ip = my_ip + "/" + str(cidr)
+
+my_network = ipaddress.ip_interface(full_ip).network
+
+for ip in my_network.hosts():
+	r = subprocess.call(['ping','-q','-c','1',str(ip)], stdout = subprocess.DEVNULL)
+
+	if r == 0:
+		print("{} is active".format(str(ip)))
+	else:
+		print("{} is not active".format(str(ip)))
